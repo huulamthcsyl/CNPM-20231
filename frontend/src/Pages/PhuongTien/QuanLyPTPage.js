ccb68771479f0e62753e17d6d35b90738efa0d79
ManTD2003 <tranduyman19503@gmail.com>
2023-10-16 13:38:37 +0700

    Add page QuanLyPTPage

--- frontend/src/Pages/PhuongTien/QuanLyPTPage.js
+++ frontend/src/Pages/PhuongTien/QuanLyPTPage.js
@@ -1,5 +1,117 @@
-function QuanLyPTPage() {
-  return <h2>Quan ly Phuong tien</h2>;
-}
+import React from "react";
+import { Grid, Button, Typography } from "@mui/material";
+import { FormControl, FormGroup, TextField } from "@mui/material";
+import { Table, TableBody, TableCell } from "@mui/material";
+import { TableRow, TableHead, TableContainer } from "@mui/material";
+import { Paper, Link } from "@mui/material";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { NavLink } from "react-router-dom";
+import PlusCircle from "../../Icons/PlusCircle.png";
 
+function QuanLyPTPage() {
+  const fields = [{ label: "Biển kiểm soát" }, { label: "Chủ sở hữu" }, { label: "Loại xe" }];
+  const tableHead = [
+    { name: "Số thứ tự" },
+    { name: "Biển kiểm soát" },
+    { name: "Loại xe" },
+    { name: "Chủ sở hữu" },
+    { name: "Ghi chú" },
+  ];
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Grid container spacing={2} style={{ padding: "50px" }}>
+        <Grid item xs={12}>
+          <h1 style={{ fontSize: "48px" }}> Quản lý phương tiện </h1>
+        </Grid>
+        <Grid item xs={6}>
+          <NavLink to="/TaoPhuongTien">
+            <Button
+              variant="contained"
+              style={{ backgroundColor: "#79C9FF", margin: "30px 0px" }}
+            >
+              <Typography style={{ marginRight: "8px" }}>
+                <img
+                  src={PlusCircle}
+                  style={{ width: "26px", height: "26px" }}
+                  alt=""
+                />
+              </Typography>
+              <Typography variant="h4" style={{ color: "black" }}>
+                Thêm phương tiện
+              </Typography>
+            </Button>
+          </NavLink>
+        </Grid>
+        <Grid item xs={12}>
+          <FormControl>
+            <FormGroup row>
+              {fields.map((field, index) => (
+                <TextField
+                  key={index}
+                  label={field.label}
+                  variant="outlined"
+                  style={{ marginRight: "35px" }}
+                  inputProps={{ style: { fontSize: "18px" } }}
+                  InputLabelProps={{ style: { fontSize: "20px" } }}
+                />
+              ))}
+            </FormGroup>
+          </FormControl>
+        </Grid>
+        
+        <Grid item>
+          <Button
+            variant="contained"
+            style={{ backgroundColor: "#79C9FF", margin: "30px 0px" }}
+          >
+            <Typography variant="h4" style={{ color: "black" }}>
+              Tìm kiếm
+            </Typography>
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }}>
+              <TableHead>
+                <TableRow>
+                  {tableHead.map((column, index) => (
+                    <TableCell key={index}>
+                      <Typography variant="h4" style={{ fontWeight: "bold" }}>
+                        {column.name}
+                      </Typography>
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell style={{ fontSize: "18px" }}>
+                    1
+                  </TableCell>
+                  <TableCell style={{ fontSize: "18px" }}>
+                    30A-88888
+                  </TableCell>
+                  <TableCell style={{ fontSize: "18px" }}>
+                    Ô tô
+                  </TableCell>
+                  <TableCell style={{ fontSize: "18px" }}>
+                    Nguyễn Văn A
+                  </TableCell>
+                  <TableCell style={{ fontSize: "18px" }}>
+                    <Link href="">
+                      Chi tiết
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+      </Grid>
+    </LocalizationProvider>
+
+  );
+
+}
 export default QuanLyPTPage;
