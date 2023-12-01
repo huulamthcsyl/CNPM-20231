9a77a8e5f50412f48ed2812bc49a4ba7747655f9
ManTD2003 <tranduyman19503@gmail.com>
2023-12-01 15:56:17 +0700

    ghep api doi mat khau

--- frontend/src/Pages/HoSo/DoiMatKhauPage.js
+++ frontend/src/Pages/HoSo/DoiMatKhauPage.js
@@ -2,7 +2,30 @@ import React from 'react'
 import { Grid, Button, Typography } from "@mui/material";
 import { TextField } from "@mui/material";
 import { NavLink } from "react-router-dom";
+import ClassApi from '../../Api/Api'
+import axios from 'axios';
+import { toast } from "react-toastify";
+import { useEffect, useState } from 'react'
 function DoiMatKhauPage() {
+
+    let MatKhau = {}
+    const [oldPassWord, setOldPassWord] = useState('');
+    const [newPassWord, setNewPassWord] = useState('');
+
+    const handleAdd = () => {
+        const id = localStorage.getItem('user')
+        ClassApi.PutPassword(id, {
+            'id': id,
+            "oldPassWord": oldPassWord,
+            "newPassWord": newPassWord,
+        }).then(
+            (response) => {
+                toast.success('thành công')
+            }
+        ).catch(() => {
+            toast.error('lỗi')
+        })
+    }
     return (
         <Grid container spacing={2} padding={"50px"}>
             <Grid item xs={12}>
@@ -17,6 +40,8 @@ function DoiMatKhauPage() {
                 <TextField
                     style={{ width: "500px" }}
                     inputProps={{ style: { fontSize: "18px" } }}
+                    value={oldPassWord}
+                    onChange={(e) => { setOldPassWord(e.target.value) }}
                 ></TextField>
             </Grid>
             <Grid item container direction="row" alignItems="center">
@@ -26,6 +51,8 @@ function DoiMatKhauPage() {
                 <TextField
                     style={{ width: "500px" }}
                     inputProps={{ style: { fontSize: "18px" } }}
+                    value={newPassWord}
+                    onChange={(e) => { setNewPassWord(e.target.value) }}
                 ></TextField>
             </Grid>
             <Grid item container direction="row" alignItems="center">
@@ -35,14 +62,16 @@ function DoiMatKhauPage() {
                 <TextField
                     style={{ width: "500px" }}
                     inputProps={{ style: { fontSize: "18px" } }}
+                    value={newPassWord}
+                    onChange={(e) => { setNewPassWord(e.target.value) }}
                 ></TextField>
             </Grid>
-
             <Grid item>
                 <NavLink to="/hosoadmin">
                     <Button
                         variant="contained"
                         style={{ backgroundColor: "#79C9FF", margin: "30px 0px" }}
+                        onClick={handleAdd}
                     >
                         <Typography variant="h4" style={{ color: "black" }}>
                             Xác nhận
